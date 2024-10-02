3d1122372eb444099c1b834fd32aa0da0953e5e0
Derek <dberb789@gmail.com>
2024-10-02 16:12:55 +0800

    chore: add userRoutes import in app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const userRoutes = require('./routes/user');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/users', userRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
