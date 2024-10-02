7bb33fd61c33d1ce8bc2581dc1a30f667b6b77ec
Derek <dberb789@gmail.com>
2024-10-02 16:20:06 +0800

    chore: import userController

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-router.post('/register');
-router.post('/login');
+router.post('/register', userController.register);
+router.post('/login', userController.login);
 router.get('/profile');
 
 module.exports = router;
