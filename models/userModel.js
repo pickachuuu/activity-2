410b646633c25fb2da17bb1c9c44f1e1e4d70206
Derek <dberb789@gmail.com>
2024-10-02 15:22:34 +0800

    feat: implemented findUser function

--- models/userModel.js
+++ models/userModel.js
@@ -5,10 +5,14 @@ const createUser = (username, password) => {
         id: users.length + 1, 
         username: username,
         password: password
-    }
+    };
 
-    users.push(newUser)
+    users.push(newUser);
     // return newUser;
+};
+
+const findUser = (username) => {
+    return users.find(user => user.username === username);
 }
 
 module.exports = {
