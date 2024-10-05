533a48bb0da095886b5e409b5a6557f5695148b9
Derek <dberb789@gmail.com>
2024-10-05 22:45:00 +0800

    feat: add rate limiter to user routes:

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const userRoutes = require('./routes/user');
+const rateLimiter = require('./middleware/rateLimiter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/users', userRoutes);
+app.use('/api/users', rateLimiter, userRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
